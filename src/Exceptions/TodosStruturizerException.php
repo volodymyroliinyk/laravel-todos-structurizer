6c3aa756e19d0bdd4cb77af689ec6c16b374d9ea
volodymyr <volodymyr.oliinyk.name@gmail.com>
2023-04-26 18:49:41 -0400

    multiple updates

---
+++ src/Exceptions/TodosStruturizerException.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace VolodymyrOliinyk\TodosStructurizer\Exceptions;
+
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Support\Arr;
+
+class TodosStructurizerException extends \Exception
+{
+
+}
--- src/Providers/PackageServiceProvider.php
+++ src/Providers/PackageServiceProvider.php
@@ -5,17 +5,22 @@ namespace VolodymyrOliinyk\TodosStructurizer\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use VolodymyrOliinyk\TodosStructurizer\Commands\TodosStructurizedCommand;
+use VolodymyrOliinyk\TodosStructurizer\Exceptions\TodosStructurizerException;
 
 final class PackageServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $configPath =  __DIR__.'/../publishable/config/todo-structurized.php';
+        $configPath =  __DIR__.'/../publishable/config/todos-structurized.php';
 
-        $this->mergeConfigFrom($configPath, 'todo-structurized');
+        if(!file_exists($configPath)){
+            throw new TodosStructurizerException(sprintf('Incorrect config path: %s', $configPath));
+        }
+
+        $this->mergeConfigFrom($configPath, 'todos-structurized');
 
         $this->publishes([
-            $configPath => config_path('todo-structurized.php'),
+            $configPath => config_path('todos-structurized.php'),
         ]);
 
         if ($this->app->runningInConsole()) {
