0007165ed65c075a8f4356e383c0b0e9abeae497
volodymyr <volodymyr.oliinyk.name@gmail.com>
2024-04-20 14:11:40 -0400

    trying to fix test, not successful

--- tests/Commands/TodosStructurizerCommandTest.php
+++ tests/Commands/TodosStructurizerCommandTest.php
@@ -3,27 +3,68 @@
 namespace VolodymyrOliinyk\TodosStructurizer\Tests\Commands;
 
 use Illuminate\Support\Facades\Artisan;
-use VolodymyrOliinyk\TodosStructurizer\Tests\TestCase;
+use PHPUnit\Framework\TestCase;
+
+//use Illuminate\Foundation\Testing\TestCase;
+//use Tests\TestCase;
+//use Tests\CreatesApplication;
+
+use VolodymyrOliinyk\TodosStructurizer\Commands\TodosStructurizerCommand;
 
 /**
  * @group todos-structurizer-command-test
- * @run vendor/bin/phpunit tests/Commands/TodosStructurizerCommandTest.php
+ * @run /usr/bin/php8.0 ./vendor/bin/phpunit tests/Commands/TodosStructurizerCommandTest.php
+ * @run /usr/bin/php8.0 ./vendor/bin/phpunit tests/Commands/TodosStructurizerCommandTest.php --verbose --debug
  */
 final class TodosStructurizerCommandTest extends TestCase
 {
+    // use CreatesApplication;
+    protected function getPackageProviders($app)
+    {
+        return \VolodymyrOliinyk\TodosStructurizer\Providers\PackageServiceProvider::class;
+    }
+
+    public function setUp(): void
+    {
+        \Illuminate\Console\Application::starting(function ($artisan) {
+            $artisan->resolveCommands([TodosStructurizerCommand::class]);
+        });
+
+        parent::setUp();
+    }
+
     /**
      * Test a console command.
      *
-     * @return void
+     * @test
      */
     public function test_console_command(): void
     {
+        echo('Test started.');
+        //  var_dump( $this->app['config']);
         /*TODO:[todos-structurizer]:
             fix tests configuration, because some error:
             1) Error: Call to undefined method Illuminate\Container\Container::basePath()
          :ENDTODO*/
 //        Artisan::call('todos-structurizer:collect')->assertExitCode(0);
-        $this->artisan('todos-structurizer:collect')->assertExitCode(0);
+
+        try {
+            echo('## marker 1.');
+            // Artisan::call('todos-structurizer:collect')
+            Artisan::call('todos-structurizer:collect');
+
+            // Capture the command's output
+            $output = Artisan::output();
+            var_dump($output);
+            // Assert or process the output as needed
+            echo('## marker 2.');
+        } catch (\Exception $exception) {
+            echo('## marker 3.');
+            var_dump($exception->getMessage());
+            var_dump($exception->getFile());
+            var_dump($exception->getLine());
+        }
+
 
         /*TODO:[todos-structurizer-demo|priority:high]:
             demo 1
@@ -49,6 +90,6 @@ final class TodosStructurizerCommandTest extends TestCase
         /*TODO:[todos-structurizer-demo|priority:low]:
      demo 5
   :ENDTODO*/
-
+        echo('Test ended.');
     }
 }
